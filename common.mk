fd1e30e6cb3d1832a4849510d5937cf7ffe5708f
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    sdm660-common: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- common.mk
+++ common.mk
@@ -37,6 +37,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/lineage-sdk
 
 # Audio
 PRODUCT_PACKAGES += \
