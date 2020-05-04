a2b7ce5e7b25ee4ffd1dcd71bf7a0d13a6d37af0
Michael Bestas <mkbestas@lineageos.org>
2020-05-04 04:15:10 +0300

    sdm660-common: Switch to stock HWC and SDM
    Change-Id: I73e059779d1b92ab03bf3865f88bebb63e0bc7f8
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- common.mk
+++ common.mk
@@ -109,7 +109,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     copybit.sdm660 \
     gralloc.sdm660 \
-    hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
     libgenlock \
