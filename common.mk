40b16c33a123406843f317d25019db5cef0970ee
Bruno Martins <bgcngm@gmail.com>
2020-12-31 00:46:08 +0000

    sdm660-common: Use CodeAurora ImsService implementation for RCS
    There is no evidence that Qualcomm devices running a rather recent IMS
    stack need to include AOSP RCS packages. RCS already worked before this
    change and continues to do so, but the main goal was to get rid of some
    of the RCS related log spam, which was accomplished here.
    
    Change-Id: I219546e29d288373877e9bcd194a60ef5e09f224

--- common.mk
+++ common.mk
@@ -325,12 +325,6 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libprotobuf-cpp-full
 
-# RCS
-PRODUCT_PACKAGES += \
-    com.android.ims.rcsmanager \
-    PresencePolling \
-    RcsService
-
 # Recovery
 TARGET_RECOVERY_DENSITY := xhdpi
 
