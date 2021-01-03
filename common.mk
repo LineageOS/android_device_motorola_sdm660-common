23bb56b0cbbcbb4f94abc3cd98c550f742e57493
Nolen Johnson <johnsonnolen@gmail.com>
2021-01-03 12:58:57 -0500

    sdm660-common: Use hardware/motorola's common MotoActions
    Change-Id: Ic730e45b25c87fb5b9d8d301bf44bd7176a0576c

--- common.mk
+++ common.mk
@@ -211,10 +211,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.sdm660
 
-# LineageActions
-PRODUCT_PACKAGES += \
-    LineageActions
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
@@ -228,6 +224,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
 
 # Network
 PRODUCT_PACKAGES += \
