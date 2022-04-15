da364d32865bcae6aa30a8bbdf0b0dbe4a37fa36
Sal Savage <salsavage@google.com>
2022-04-15 15:38:17 -0700

    sdm660-common: Replace BTM_DEF_LOCAL_NAME with a sysprop
    Change-Id: I2c90efac01e61500eb1be23bb0ae6f96c2acdc53

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,39 +23,8 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#pragma push_macro("PROPERTY_VALUE_MAX")
-
-#include <cutils/properties.h>
-#include <string.h>
-
-static inline const char* BtmGetDefaultName()
-{
-    char product_device[PROPERTY_VALUE_MAX];
-    property_get("ro.product.device", product_device, "");
-
-    if (strstr(product_device, "beckham"))
-        return "Moto Z3 Play";
-    if (strstr(product_device, "chef"))
-        return "Moto One Power";
-    if (strstr(product_device, "evert"))
-        return "Moto G6 Plus";
-    if (strstr(product_device, "lake"))
-        return "Moto G7 Plus";
-    if (strstr(product_device, "lake_n"))
-        return "REVVLRY+";
-    if (strstr(product_device, "payton"))
-        return "Moto X4";
-
-    // Fallback to Moto SDM Generic
-    return "Moto SDM6xx";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
-
 #define BLE_VND_INCLUDED   TRUE
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
-
-#pragma pop_macro("PROPERTY_VALUE_MAX")
 
 #endif
